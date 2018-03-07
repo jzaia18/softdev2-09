962b279eb742efae42c5fa100f97efd1f5c1c5f2
Jake Zaia <jzaia@stuy.edu>
2018-03-07 18:22:18 -0500

    Finish work

--- svg.js
+++ svg.js
@@ -1,6 +1,30 @@
 var clear = function() {
-    //todo
-}
+  while (slate.children[0])
+    slate.removeChild(slate.children[0]);
+};
 
-var slate = document.getElementById("slate")
-document.getElementById("clear").addEventListener("click", clear)
+var addCirc = function(e) {
+  var circ = document.createElementNS(NS, "circle");
+  circ.setAttribute("cx", e.offsetX);
+  circ.setAttribute("cy", e.offsetY);
+  circ.setAttribute("r", 20);
+  circ.setAttribute("fill", "lightsteelblue");
+  circ.addEventListener("click", circhameleon);
+  slate.appendChild(circ);
+};
+
+var circhameleon = function(e) {
+  e.stopPropagation();
+  if (e.srcElement.getAttribute("fill") == "palevioletred") {
+    slate.removeChild(e.srcElement);
+    addCirc({offsetX: Math.random() * 500, offsetY: Math.random() * 500}); //cheaty but legal
+  }
+  else
+    e.srcElement.setAttribute("fill", "palevioletred");
+};
+
+var NS = "http://www.w3.org/2000/svg";
+
+var slate = document.getElementById("slate");
+slate.addEventListener("click", addCirc);
+document.getElementById("clear").addEventListener("click", clear);
